ab934534db98c8b9356c39170305efe8243c8917
Jaacksooon <jackson215rodrigues@gmail.com>
2025-10-15 15:21:06 -0300

    meu primeiro projeto

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-let nome : "jackson"
-console.log(nome)
+let nomeDeHeroi = "Jadson"
+let nivelDeExperiencia = 3
+
+ switch (nivelDeExperiencia) { 
+
+case nivelDeExperiencia =1: 
+console.log ( nomeDeHeroi + " esta no nivel: prata"  ) 
+break
+
+case nivelDeExperiencia =2:
+    console.log (nomeDeHeroi + " esta no nivel: ouro"  )
+    break
+
+ case nivelDeExperiencia =3:
+        console.log (nomeDeHeroi + " esta no nivel: platina"  )
+       break
+
+       default:
+        console.log (nomeDeHeroi + " nivel indefinido")
+ }
+
